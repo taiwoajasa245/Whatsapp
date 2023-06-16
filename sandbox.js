eb9481b7ae9a09f2690d85d3c5ef6058ed65c62b
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-06-16 22:17:51 +0100

    loader created

---
+++ sandbox.js
@@ -0,0 +1,32 @@
+const messageInput = document.getElementById('message-input'); 
+const leftSideMainPage = document.getElementById('l-s-main'); 
+
+
+const allSection = document.querySelector('.all-section');
+const loader = document.querySelector('.loader'); 
+
+//// loader
+
+window.addEventListener('load', move()); 
+function move(){ 
+    const elem = document.getElementById('bar'); 
+    elem.style.width = "30em";  
+}
+
+window.addEventListener('load', () => { 
+     allSection.style.display = ' none'; 
+     
+    setTimeout(() => {
+        allSection.style.display = 'flex'; 
+        loader.style.display = 'none'; 
+    }, 10000);
+
+})
+
+// message
+
+function messageDiv(){ 
+    messageInput.style.display = 'block'; 
+    leftSideMainPage.style.display = 'none'; 
+}
+
