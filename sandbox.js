e2df65895dfde082684fb0e90c7a7f16e6bc945f
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-06-20 20:03:44 +0100

    changes made to the loaders

--- sandbox.js
+++ sandbox.js
@@ -6,12 +6,15 @@ const allSection = document.querySelector('.all-section');
 const loader = document.querySelector('.loader'); 
 const loaderTwo = document.querySelector('.loader_2'); 
 
+
 //// loader
 
 window.addEventListener('load', move()); 
 function move(){ 
     const elem = document.getElementById('bar'); 
+    const loaderSvg = document.querySelector('.loader-svg'); 
     elem.style.width = "30em";  
+    loaderSvg.style.height = "70px";
 }
 
 window.addEventListener('load', () => { 
@@ -22,26 +25,36 @@ window.addEventListener('load', () => {
         // allSection.style.display = ''; 
         loader.style.display = 'none'; 
         loaderTwo.style.display = "block"; 
-    }, 1000);
+    }, 10000);
 
 })
 
 //// loader_2
+// function moveTwo(){ 
+//     const element = document.getElementById('bar_2');
+//     console.log(element); 
+    
+//     element.style.width = "20em";  
+// } 
 
-window.addEventListener('load', move_2()); 
-function move_2(){ 
-    const elem = document.getElementById('bar_2'); 
-    elem.style.width = "30em";  
+window.addEventListener('load', moveTwo()); 
+function moveTwo(){ 
+    const element = document.getElementById('bar_2'); 
+    
+    element.style.width = "30em";  
 }
 
 window.addEventListener('load', () => { 
    
+   
+  
     
      
     setTimeout(() => {
         allSection.style.display = 'flex'; 
         // loader.style.display = 'none'; 
         loaderTwo.style.display = "none"; 
+     
     }, 100000);
 
 })
