994135545b6ec156ee7a514db11634b055986f13
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-06-20 19:11:08 +0100

    new loader added

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,7 @@ const leftSideMainPage = document.getElementById('l-s-main');
 
 const allSection = document.querySelector('.all-section');
 const loader = document.querySelector('.loader'); 
+const loaderTwo = document.querySelector('.loader_2'); 
 
 //// loader
 
@@ -15,13 +16,37 @@ function move(){
 
 window.addEventListener('load', () => { 
      allSection.style.display = ' none'; 
+     loaderTwo.style.display = "none";
      
     setTimeout(() => {
-        allSection.style.display = 'flex'; 
+        // allSection.style.display = ''; 
         loader.style.display = 'none'; 
+        loaderTwo.style.display = "block"; 
     }, 1000);
 
 })
+
+//// loader_2
+
+window.addEventListener('load', move_2()); 
+function move_2(){ 
+    const elem = document.getElementById('bar_2'); 
+    elem.style.width = "30em";  
+}
+
+window.addEventListener('load', () => { 
+   
+    
+     
+    setTimeout(() => {
+        allSection.style.display = 'flex'; 
+        // loader.style.display = 'none'; 
+        loaderTwo.style.display = "none"; 
+    }, 100000);
+
+})
+
+
 
 // message
 
