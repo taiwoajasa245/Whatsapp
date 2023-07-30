5ff642b4cf94b7d01bd0b5765940a4618a285b11
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-07-30 23:58:40 +0100

    code updated

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 
 // hello world
+
 console.log("Hello JavaScript"); 
 
 const leftSideMainPage = document.getElementById('l-s-main'); 
@@ -7,57 +8,57 @@ const allSection = document.querySelector('.all-section');
 const loader = document.querySelector('.loader'); 
 const loaderTwo = document.querySelector('.loader_2'); 
 
-// //// loader
+//// loader
 
-// window.addEventListener('load', move()); 
-// function move(){ 
-//     const elem = document.getElementById('bar'); 
-//     const loaderSvg = document.querySelector('.loader-svg'); 
-//     elem.style.width = "30em";  
-//     loaderSvg.style.height = "70px";
-// }
+window.addEventListener('load', move()); 
+function move(){ 
+    const elem = document.getElementById('bar'); 
+    const loaderSvg = document.querySelector('.loader-svg'); 
+    elem.style.width = "30em";  
+    loaderSvg.style.height = "70px";
+}
 
-// window.addEventListener('load', () => { 
-    //      allSection.style.display = ' none'; 
-    //      loaderTwo.style.display = "none";
+window.addEventListener('load', () => { 
+         allSection.style.display = ' none'; 
+         loaderTwo.style.display = "none";
      
-    //     setTimeout(() => {
-        //         // allSection.style.display = ''; 
-        //         loader.style.display = 'none'; 
-        //         loaderTwo.style.display = "block"; 
-        //     }, 10000);
+        setTimeout(() => {
+                // allSection.style.display = ''; 
+                loader.style.display = 'none'; 
+                loaderTwo.style.display = "block"; 
+            }, 10000);
 
-        // })
+        })
         
-        // //// loader_2
-        // function moveTwo(){ 
-            //     const element = document.getElementById('bar_2');
-//     console.log(element); 
+        //// loader_2
+        function moveTwo(){ 
+                const element = document.getElementById('bar_2');
+    console.log(element); 
 
-//     element.style.width = "30em";  
-// } 
+    element.style.width = "30em";  
+} 
 
-// // window.addEventListener('load', moveTwo()); 
-// // function moveTwo(){ 
-    // //     const element = document.getElementById('bar_2'); 
+// window.addEventListener('load', moveTwo()); 
+// function moveTwo(){ 
+    //     const element = document.getElementById('bar_2'); 
     
-    // //     element.style.width = "30em";  
-    // // }
+    //     element.style.width = "30em";  
+    // }
     
-    // window.addEventListener('load', () => { 
+    window.addEventListener('load', () => { 
         
-        //     setTimeout(() => {
-//         moveTwo(); 
-//     }, 20000);
+            setTimeout(() => {
+        moveTwo(); 
+    }, 20000);
 
-//     setTimeout(() => {
-//         allSection.style.display = 'flex'; 
-//         loader.style.display = 'none'; 
-//         loaderTwo.style.display = "none"; 
+    setTimeout(() => {
+        allSection.style.display = 'flex'; 
+        loader.style.display = 'none'; 
+        loaderTwo.style.display = "none"; 
      
-//     }, 12000);
+    }, 12000);
 
-// }); 
+}); 
 
 
 
